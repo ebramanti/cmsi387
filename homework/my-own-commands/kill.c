38114b79bc042031ebe641d54e62a6c0abeb0adf
Edward Bramanti <edward@bramanti.org>
2014-02-20 02:12:30 -0800

    Finished main of kill.c, beginning testing.

--- homework/my-own-commands/kill.c
+++ homework/my-own-commands/kill.c
@@ -1,6 +1,13 @@
+//	kill.c, an implementation of a system call in C/
+//	Man page: http://linux.die.net/man/2/kill
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (int argc, char *argv[]) {
     int result = syscall (37, atoi(argv[1]), atoi(argv[2]));
+
+    if (result == -1) {
+    	printf("kill: %s: %s\n", argv[1], "Error killing process.");
+  	}
 }
