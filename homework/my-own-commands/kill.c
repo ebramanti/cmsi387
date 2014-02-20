5fd739199792bb141fd8fc66ce9879fe6cf97a5d
Edward Bramanti <edward@bramanti.org>
2014-02-20 02:39:10 -0800

    Testing for 0 return value on success.

--- homework/my-own-commands/kill.c
+++ homework/my-own-commands/kill.c
@@ -6,7 +6,7 @@
 
 int main (int argc, char *argv[]) {
     int result = syscall (37, atoi(argv[1]), atoi(argv[2]));
-
+    printf("%d\n", result);
     if (result == -1) {
     	printf("kill: %s: %s\n", argv[1], "Error killing process.");
   	}
