ba361eff2a9e5c398a40ba0fdb16f554aa9ad0cc
Edward Bramanti <edward@bramanti.org>
2014-02-20 03:06:36 -0800

    Taking a pause from open.c, seems to be working.

--- homework/my-own-commands/open.c
+++ homework/my-own-commands/open.c
@@ -1,7 +1,7 @@
 //  open.c, an implementation of a system call in C.
 //  Man page: http://linux.die.net/man/3/open
 
-#include <sys/stat.h> // was listed on man page, testing to see if necessary.
+#include <sys/stat.h> // was listed on man page, including.
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,6 +11,6 @@ int main (int argc, char *argv[]) {
 
     //  If result is -1, an error message is thrown.
     if (result == -1) {
-        printf("open: %s: %s\n", argv[1], "Error opening/modifying process.");
+        printf("open: %s: %s\n", argv[1], "Error opening.");
     }
 }
