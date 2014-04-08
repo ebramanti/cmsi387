1707e34d5152a1e3079c1c684732dfe44716c7a8
Edward Bramanti <edward@bramanti.org>
2014-04-08 01:40:05 -0700

    Shell is successfully looping, switched to execvp to test a command.

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -1,8 +1,10 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define MAX_COMMAND_LENGTH 256
+#define MAX_ARGS_LENGTH 256
 
 /**
  * This program demonstrates the use of the fork() and exec()
@@ -10,33 +12,41 @@
  */
 
 int parseCommand(char* command, char* args) {
-    
+
 }
 
 int main() {
     /* String to hold the command to run. */
     char command[MAX_COMMAND_LENGTH];
+    char* args[MAX_ARGS_LENGTH];
+
+
     printf("Enter the command to run: ");
-    scanf("%s", command);
+    
+    while(!feof(stdin)) {
+        fgets(command, MAX_COMMAND_LENGTH, stdin);
+        printf("%s\n", command);
 
-    /* Variable that will store the fork result. */
-    pid_t pid;
+        /* Variable that will store the fork result. */
+        pid_t pid;
 
-    /* Perform the actual fork. */
-    pid = fork();
-    if (pid < 0) {
-        /* Error condition. */
-        fprintf(stderr, "Fork failed\n");
-        return -1;
-    } else if (pid == 0) {
-        /* Child process. */
-        printf("Running...\n");
-        execlp(command, command, NULL);
-    } else {
-        /* Parent process. */
-        int result;
-        wait(&result);
-        printf("All done; result = %d\n", result);
+        /* Perform the actual fork. */
+        pid = fork();
+        if (pid < 0) {
+            /* Error condition. */
+            fprintf(stderr, "Fork failed\n");
+            return -1;
+        } else if (pid == 0) {
+            /* Child process. */
+            printf("Running...\n");
+            args[0] = "ls";
+            execvp(args[0], args);
+        } else {
+            /* Parent process. */
+            int result;
+            wait(&result);
+            printf("All done; result = %d\n", result);
+        }
     }
 
     return 0;
