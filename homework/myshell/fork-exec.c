c7dbce48b9fc2a6ef10fde5acac975e548697dcc
Edward Bramanti <edward@bramanti.org>
2014-04-08 04:43:11 -0700

    Implemented empty command loop, cleaned up and finalized shell.

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -29,60 +29,60 @@ int main() {
     /* String to hold the command to run. */
     char command[MAX_COMMAND_LENGTH];
     char* args[MAX_ARGS_LENGTH];
-
-
-    printf("Enter the command to run:\n");
+    printf("CUSTOM SHELL => Enter the command to run:\n");
 
     while(!feof(stdin)) {
         fputs(SHELL_INDICATOR, stdout);
         fgets(command, MAX_COMMAND_LENGTH, stdin);
+        // Terminating null for end of command.
+        command[strlen(command) - 1] = '\0';
         int commandLength = strlen(command);
 
-        // Terminating null for end of command.
-        command[commandLength - 1] = '\0';
-        // Delete trailing whitespace.
-        while(strcmp(&command[commandLength - 2], DELIMITER) == 0) {
-            command[commandLength - 2] = '\0';
-            commandLength--;
-        }
-        // A check for the concurrency command.
-        int waitCheck = 0;
-        while(strcmp(&command[commandLength - 2], WAIT_CHARACTER) == 0) {
-            command[commandLength - 2] = '\0';
-            waitCheck = 1;
-        }
+        if (commandLength > 0) {
+            // Delete trailing whitespace.
+            while(strcmp(&command[commandLength - 2], DELIMITER) == 0) {
+                command[commandLength - 2] = '\0';
+                commandLength--;
+            }
+            // A check for the concurrency command.
+            int waitCheck = 0;
+            while(strcmp(&command[commandLength - 2], WAIT_CHARACTER) == 0) {
+                command[commandLength - 2] = '\0';
+                waitCheck = 1;
+            }
 
-        parseCommand(command, args);
-        if (strcmp(args[0], "exit") == 0) {
-            return; // Exit the shell.
-        } else if (strcmp(args[0], "cd") == 0) {
-            //http://www.tutorialspoint.com/unix_system_calls/chdir.htm
-            chdir(args[1]);
-        } else if (strcmp(args[0], "secret-system-call") == 0) {
-            // Since my kernel was designed for a phone, this will just print a message.
-            // An example of what I would call is shown below.
-            // syscall(378);
-            char* androidMessage = "This would normally print my secret system call on a Nexus 4 running my custom-built Android kernel.\n";
-            fputs(androidMessage, stdout);
-        } else {
-            /* Variable that will store the fork result. */
-            pid_t pid;
-
-            /* Perform the actual fork. */
-            pid = fork();
-            if (pid < 0) {
-                /* Error condition. */
-                fprintf(stderr, "Fork failed\n");
-                return -1;
-            } else if (pid == 0) {
-                /* Child process. */
-                execvp(args[0], args);
+            parseCommand(command, args);
+            if (strcmp(args[0], "exit") == 0) {
+                return; // Exit the shell.
+            } else if (strcmp(args[0], "cd") == 0) {
+                //http://www.tutorialspoint.com/unix_system_calls/chdir.htm
+                chdir(args[1]);
+            } else if (strcmp(args[0], "secret-system-call") == 0) {
+                // Since my kernel was designed for a phone, this will just print a message.
+                // An example of what I would call is shown below.
+                // syscall(378);
+                char* androidMessage = "This would normally print my secret system call on a Nexus 4 running my custom-built Android kernel.\n";
+                fputs(androidMessage, stdout);
             } else {
-                /* Parent process. */
-                int result;
-                //  Do not stop process if wait command is used.
-                if (!waitCheck) {
-                    wait(&result);
+                /* Variable that will store the fork result. */
+                pid_t pid;
+
+                /* Perform the actual fork. */
+                pid = fork();
+                if (pid < 0) {
+                    /* Error condition. */
+                    fprintf(stderr, "Fork failed\n");
+                    return -1;
+                } else if (pid == 0) {
+                    /* Child process. */
+                    execvp(args[0], args);
+                } else {
+                    /* Parent process. */
+                    int result;
+                    //  Do not stop process if wait command is used.
+                    if (!waitCheck) {
+                        wait(&result);
+                    }
                 }
             }
         }
