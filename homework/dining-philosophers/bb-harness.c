6fafb054a03b7dcf22e01b4bffb447266df3f2c1
Edward Bramanti <edward@bramanti.org>
2014-04-29 17:21:46 -0700

    Setting up initialization for dining philosopher

--- homework/dining-philosophers/bb-harness.c
+++ homework/dining-philosophers/bb-harness.c
@@ -6,27 +6,29 @@
 
 #include "bb-sync.h"
 
+// Number of philosophers and chopsticks.
+#define NUM 5
+
+#define THINKING 0
+#define HAS_CHOPSTICK 1
+#define EATING 2
+
+int chopstick_state[NUM];
+int philosopher_state[NUM];
+
+pthread_t philosopher[NUM];
+pthread_mutex_t chopsticks[NUM];
+
 /* The test harness defines the expected interfaces. */
 void *produce(int produceBound);
 void *consume(int consumeBound);
 
 /**
- * Thread runner for the producer.
- */
-void *prodRunner(void *arg) {
-    printf("Starting producer\n");
-    produce((int)arg);
-    printf("Ending producer\n");
-    pthread_exit(NULL);
-}
-
-/**
  * Thread runner for the consumer.
  */
-void *consRunner(void *arg) {
-    printf("Starting consumer\n");
-    consume((int)arg);
-    printf("Ending consumer\n");
+void *chopstickRunner(void *arg, void *arg2) {
+    //printf("Starting consumer\n");
+    //printf("Ending consumer\n");
     pthread_exit(NULL);
 }
 
@@ -34,19 +36,29 @@ void *consRunner(void *arg) {
  * Overall driver program.
  */
 int main(int argc, char** argv) {
-    printf("Starting bounded buffer test harness...\n");
+    printf("Starting Dining Philosophers Program...\n");
 
     /* Initialize the synchronization primitives. */
     initSync();
 
     /* Start the threads. */
-    pthread_t prod, cons;
+
+    for (int i = 0; i < NUM; i++) {
+        philosopher_state[i] = THINKING;
+        chopstick_state[i] = 0;
+        pthread_create(&philosophers[i], NULL,/*need value here*/, i);
+        pthread_mutex_init(&chopsticks[i], NULL);
+    }
+
+
+    /*pthread_t prod, cons;
     pthread_create(&prod, NULL, prodRunner, (void *)2);
-    pthread_create(&cons, NULL, consRunner, (void *)3);
+    pthread_create(&cons, NULL, consRunner, (void *)3); */
 
     /* Not really needed, since we'll pretty much run forever... */
-    pthread_join(prod, NULL);
-    pthread_join(cons, NULL);
+    for (int i = 0; i < NUM; i++) {
+        pthread_join(philosophers[i], NULL);
+    }
 
     /* We won't really get here, but to avoid any warnings... */
     return 0;
