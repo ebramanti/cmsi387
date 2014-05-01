d65a004ce8e2f37e351c251c5ee03e470d1bf598
Edward Bramanti <edward@bramanti.org>
2014-05-01 03:26:29 -0700

    Completed Dining Philosophers Problem

--- homework/dining-philosophers/philosopher.c
+++ homework/dining-philosophers/philosopher.c
@@ -1,32 +1,64 @@
 /**
- * Philosopher library implementation.
+ * An implementation of dining philosophers.
  */
 #include "utility.h"
-
 #include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 
 #define NUM_OF_PHILS 5
-
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
 
+pthread_t philosophers[NUM_OF_PHILS];
+pthread_mutex_t chopsticks[NUM_OF_PHILS];
+int philosopher_state[NUM_OF_PHILS];
+int chopstick_state[NUM_OF_PHILS];
+
 void display_philosopher() {
     for (int i = 0; i < NUM_OF_PHILS; i++) {
-        if (philosopher_state[i] == THINKING) {
+        int currentState = philosopher_state[i];
+        if (currentState == THINKING) {
             printf("THINKING ");
-        } else if (philosopher_state[i] == HUNGRY) {
+        } else if (currentState == HUNGRY) {
             printf("HUNGRY ");
-        } else if (philosopher_state[i] == EATING) {
+        } else if (currentState == EATING) {
             printf("EATING ");
         }
     }
+    printf("\n");
+}
+
+int releaseChopstick(int chopstick) {
+    if (chopstick > NUM_OF_PHILS) {
+        printf("Index out of bounds.\n");
+        return -1;
+    }
+    else if (chopstick_state[chopstick] == 0) {
+        printf("Chopstick %d not taken.\n", chopstick);
+        return -1;
+    }
+    pthread_mutex_unlock(&chopsticks[chopstick]);
+    chopstick_state[chopstick] -= 1;
+    return 0;
+}
+
+int getChopstick(int chopstick) {
+    if (chopstick > NUM_OF_PHILS) {
+        printf("Index out of bounds.\n");
+        return -1;
+    }
+    else if (chopstick_state[chopstick] == 1) {
+        printf("Chopstick %d already taken.\n", chopstick);
+        return -1;
+    }
+    pthread_mutex_lock(&chopsticks[chopstick]);
+    chopstick_state[chopstick] += 1;
+    return 0;
 }
 
 void think(int philosopher) {
-    randomWait(NUM_OF_PHILS);
+    randomwait(NUM_OF_PHILS);
     philosopher_state[philosopher] = HUNGRY;
 }
 
@@ -34,7 +66,7 @@ void hungry(int philosopher) {
     getChopstick(philosopher);
     getChopstick((philosopher + 1) % NUM_OF_PHILS);
     philosopher_state[philosopher] = EATING;
-    randomWait(NUM_OF_PHILS);
+    randomwait(NUM_OF_PHILS);
 }
 
 void eating(int philosopher) {
@@ -43,50 +75,42 @@ void eating(int philosopher) {
     philosopher_state[philosopher] = THINKING;
 }
 
-int getChopstick(int chopstick) {
-    if (chopstick > NUM_OF_PHILS) {
-        printf("Index out of bounds.");
-        return -1;
-    }
-    else if (chopstickState[chopstick] == 1) {
-        printf("Chopstick %d already taken.", chopstick);
-        return -1;
-    }
-    pthread_mutex_lock(&chopsticks[chopstick]);
-    chopsticks[chopstick] += 1;
-    return 0;
-}
-
-int releaseChopstick(int chopstick) {
-    if (chopstick > NUM_OF_PHILS) {
-        printf("Index out of bounds.");
-        return -1;
-    }
-    else if (chopstickState[chopstick] == 0) {
-        printf("Chopstick %d not taken.", chopstick);
-        return -1;
-    }
-    pthread_mutex_unlock(&chopsticks[chopstick]);
-    chopsticks[chopstick] -= 1;
-    return 0;
-}
-
-void run_philosopher(int philosopher) {
-    display_philosopher();
-    int* currentState = philosopher_state[philosopher];
-    // STILL UNIMPLEMENTED.
+void* run_philosopher(void* philosopher) {
+    int index = *(int*) philosopher;
     while (1) {
-        display_philosopher();
-        if (currentState == THINKING) {
-            think(philosopher);
-        } else if (currentState == HUNGRY) {
-            hungry(philosopher);
-        } else if (currentState == HAS_CHOP) {
-            has_one_chop(philosopher);
-        } else if (currentState == EATING) {
-            eating(philosopher);
+        if (philosopher_state[index] == EATING) {
+            eating(index);
+        } else if (philosopher_state[index] == THINKING) {
+            think(index);
+        } else if (philosopher_state[index] == HUNGRY) {
+            hungry(index);
         } else {
             // THIS IS BAD.
         }
+        display_philosopher();
     }
+}
+
+/**
+ * Overall driver program.
+ */
+int main () {
+    printf("Starting Dining Philosophers Program...\n");
+    /* Create threads and initialize mutexes. */
+    int index[NUM_OF_PHILS];
+    for (int i = 0; i < NUM_OF_PHILS; i++) {
+        index[i] = i;
+        philosopher_state[i] = THINKING;
+        chopstick_state[i] = 0;
+        pthread_create(&philosophers[i], NULL, run_philosopher, &index[i]);
+        pthread_mutex_init(&chopsticks[i], NULL);
+    }
+
+    /* Not really needed, since we'll pretty much run forever... */
+    for (int i = 0; i < NUM_OF_PHILS; i++) {
+        pthread_join(philosophers[i], NULL);
+    }
+
+    /* We won't really get here, but to avoid any warnings... */
+    return 0;
 }
